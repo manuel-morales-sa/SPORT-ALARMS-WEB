f5ce884ae18d426f75296e3fa76f87addedaa203
AndresAlba <o.alba@uniandes.edu.co>
2023-03-16 08:30:12 -0500

    delete unused components

--- src/app/alarmas/alarmas.module.ts
+++ src/app/alarmas/alarmas.module.ts
@@ -4,13 +4,11 @@ import { MatDividerModule } from '@angular/material/divider';
 import { MatCardModule } from '@angular/material/card';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatRadioModule } from '@angular/material/radio';
-import { MenuComponent } from '../menu/menu.component';
-import { SettingsComponent } from './settings/settings.component';
 import { NgModule } from '@angular/core';
 import { ComponentsModule } from "../components/components.module";
 
 @NgModule({
-    declarations: [AlarmasComponent, SettingsComponent, MenuComponent],
+    declarations: [AlarmasComponent],
     imports: [
       CommonModule,
         MatDividerModule,
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { SignupComponent } from './user/signup/signup.component';
 import { LoginComponent } from './user/login/login.component';
 import { AlarmasComponent } from './alarmas/alarmas/alarmas.component';
-import { SettingsComponent } from './alarmas/settings/settings.component';
 import { ForgotComponent } from './user/forgot/forgot.component';
 
 const routes: Routes = [
@@ -22,23 +21,23 @@ const routes: Routes = [
     component: SignupComponent,
     pathMatch: 'full'
   },
-
   {
     path: 'forgot',
     component:  ForgotComponent,
     pathMatch: 'full'
   },
   {
-    path: 'settings',
-    component: SettingsComponent,
-    pathMatch: 'full'
-  },
-
-  {
     path: 'alarm',
     component: AlarmasComponent,
     pathMatch: 'full'
   },
+  {
+    path: '', 
+    component: LoginComponent
+  },
+  {
+    path: '**', 
+    component: LoginComponent}
 ];
 
 @NgModule({
